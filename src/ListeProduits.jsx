9c0ee5f5446ec05cafd964ac134ec967ac590af3
Camille Semaan <cdem@xvitae.com>
2021-02-04 16:58:21 -0500

    Liste de produits dynamique (fichier JSON)

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,13 +1,22 @@
 import './ListeProduits.css';
 import Produit from "./Produit";
+import tabProduits from './data/produits.json';
 
 export default function ListeProduits(props) {
+
+  let tab = [1,2,3];
+  let tabDouble = tab.map(function(x){return 2*x}); //[2,4,6]
+
   return (
     <>
       <h2>Produits disponibles</h2>
       <ul className="ListeProduits">
-        <Produit nom="T-Shirt col rond" prix="19.99" id="prd0004" />
-        <Produit nom="Chandail à manche longue" prix="25.50" id="prd0002" />
+        {/* On va générer les produits à partir de la 
+          structure obtenue du fichier JSON des produits */}
+        
+        {tabProduits.map(prd => 
+          <Produit nom={prd.nom} prix={prd.prix} id={prd.id} />
+        )}
       </ul>
     </>
   );
